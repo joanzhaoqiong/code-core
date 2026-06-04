3cca112bfe204751b643fab8ff167d627495b191
joanzhaoqiong <2657349736.qq.com>
2026-06-05 00:27:59 +0800

    my first git

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-// test my git
+        // test my git
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {//TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
